6f3bdd72913f0447db0da75df77fd0c9fc5b2da4
muhammad-shahadat <161308541+muhammad-shahadat@users.noreply.github.com>
2024-04-28 15:02:12 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,43 @@
+const product = document.querySelectorAll(".product");
+let x = 0;
+let movePerClick = 30;
+let maxMove = 225;
+
+let mobileView = window.matchMedia("(max-width:768px)");
+if (mobileView.matches) {
+    movePerClick = 100;
+    maxMove = 600;
+}
+
+
+let prevSlider = ()=>{
+    x = x - movePerClick;
+    for (let i= 0; i<product.length; i++) {
+        // @ts-ignore
+        product[i].style.left = "-" + x + "%";
+       
+    }
+   
+}
+let nextSlider = () =>{
+    if (x >=maxMove) {
+        x = 0;
+        if (x=0) {
+            x = x+movePerClick;
+        }
+        
+    }
+    
+    else{
+        x = x + movePerClick;
+    }
+
+    
+    for(let i = 0; i<product.length; i++) {
+        // @ts-ignore
+        product[i].style.left = "-" + x + "%";
+        
+    }
+   
+}
+
